d13ff8a7540365581640180adc95c7ed634d3b11
alejandroc4lderon <alejandroc4lderon@gmail.com>
2024-07-30 22:32:19 -0500

    Primer hola mundo

---
+++ hello_python.py
@@ -0,0 +1,9 @@
+#programa para sumar dos numeros
+
+numero1 = int(input("digita primer numero"))
+numero2 = int(input("digita el segundo numero"))
+
+resultado = numero1 + numero2
+print(resultado)
+
+
---
+++ numero_par.py
@@ -0,0 +1,7 @@
+# Programa para saber si un numero es par o impar
+
+numero = int(input("digita numero"))
+if numero % 2 == 0:
+    print("el numero es par")
+else:
+    print("el numero es impar")
